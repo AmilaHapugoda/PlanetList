b3dd23f28c8deb533de2e597f6bb273498372637
Amila Prasad <mailofamila@gmail.com>
2022-09-11 09:33:34 +0530

    Initial Commit

---
+++ PlanetList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlanetList
+//
+//  Created by Amila Prasad on 2022-09-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
